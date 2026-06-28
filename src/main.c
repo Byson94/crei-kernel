e3a04b95a2a52827f535808b8b3b9f80b570c70f
Byson94 <byson94@outlook.com>
2026-06-28 21:41:45 +0530

    feat: move shell into its own module

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "io.h"
+#include "shell.h"
 
 int main() {
     print_string("Hello from Crei!\n");
@@ -6,28 +7,7 @@ int main() {
     line_start_position = cursor_position;
 
     while(1) {
-        unsigned char status = inb(0x64);
-
-        if (status & 0x01) {
-            unsigned char scancode = inb(0x60);
-            
-            if (scancode < 0x80) {
-                volatile char *video_memory = (volatile char*) 0xB8000;
-                int i = 0;
-                
-                while (global_ibm_array[i].scancode != 0x0) {
-                    if (global_ibm_array[i].scancode == scancode) {
-                        print_string((char*)global_ibm_array[i].ascii);
-                        if (global_ibm_array[i].ascii[0] == '\n') {
-                            print_string("$ ");
-                            line_start_position = cursor_position;
-                        }
-                        break;
-                    }
-                    i++;
-                }
-            }
-        }
+        run_shell();
     }
 
     return 0;
---
+++ src/shell.c
@@ -0,0 +1,27 @@
+#include "io.h"
+#include "shell.h"
+
+void run_shell() {
+    unsigned char status = inb(0x64);
+
+    if (status & 0x01) {
+        unsigned char scancode = inb(0x60);
+        
+        if (scancode < 0x80) {
+            volatile char *video_memory = (volatile char*) 0xB8000;
+            int i = 0;
+            
+            while (global_ibm_array[i].scancode != 0x0) {
+                if (global_ibm_array[i].scancode == scancode) {
+                    print_string((char*)global_ibm_array[i].ascii);
+                    if (global_ibm_array[i].ascii[0] == '\n') {
+                        print_string("$ ");
+                        line_start_position = cursor_position;
+                    }
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
