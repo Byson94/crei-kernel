231f5989447ad3ae4105b8f735f5a23e7714a922
Byson94 <byson94@outlook.com>
2026-06-28 21:23:34 +0530

    feat: better print_string handling

--- src/io.c
+++ src/io.c
@@ -17,12 +17,12 @@ IBMPair global_ibm_array[] = {
     {0x02, "1"},  {0x03, "2"},  {0x04, "3"},  {0x05, "4"},
     {0x06, "5"},  {0x07, "6"},  {0x08, "7"},  {0x09, "8"},
     {0x0A, "9"},  {0x0B, "0"},  {0x0C, "-"},  {0x0D, "="},
-    {0x0E, "^H"}, // Backspace
-    {0x0F, "^I"}, // Tab
+    {0x0E, "\b"}, // Backspace
+    {0x0F, "\t"}, // Tab
     {0x10, "q"},  {0x11, "w"},  {0x12, "e"},  {0x13, "r"},
     {0x14, "t"},  {0x15, "y"},  {0x16, "u"},  {0x17, "i"},
     {0x18, "o"},  {0x19, "p"},  {0x1A, "["},  {0x1B, "]"},
-    {0x1C, "^M"}, // Enter
+    {0x1C, "\n"}, // Enter
     {0x1E, "a"},  {0x1F, "s"},  {0x20, "d"},  {0x21, "f"},
     {0x22, "g"},  {0x23, "h"},  {0x24, "j"},  {0x25, "k"},
     {0x26, "l"},  {0x27, ";"},  {0x28, "'"},  {0x29, "`"},
@@ -34,15 +34,23 @@ IBMPair global_ibm_array[] = {
 
 void print_string(char *string_ptr) {
     char *str = string_ptr;
-    
     volatile char *video_memory = (volatile char*) 0xB8000;
     
     int i = 0;
     while (str[i] != '\0') {
-        video_memory[cursor_position * 2] = str[i];
-        video_memory[cursor_position * 2 + 1] = 0x0F;
-        
-        cursor_position++;
+        if (str[i] == '\n') {
+            cursor_position += (80 - (cursor_position % 80));
+        } else if (str[i] == '\b') {
+            cursor_position--;
+            video_memory[cursor_position * 2] = ' ';
+        } else if (str[i] == '\t') {
+            cursor_position += (4 - (cursor_position % 4));
+        } else {
+            video_memory[cursor_position * 2] = str[i];
+            video_memory[cursor_position * 2 + 1] = 0x0F;
+            cursor_position++;
+        }
+
         i++;
     }
     update_hardware_cursor(cursor_position);
