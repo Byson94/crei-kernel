2fadac7b8d7fe7d1718cd20bb7e05a0c51e07caa
Byson94 <byson94@outlook.com>
2026-06-28 19:58:03 +0530

    feat: add printing functionality

---
+++ README.md
@@ -0,0 +1,15 @@
+# Crei Kernel
+
+A minimal kernel written in C. Nothing big, not the next linux, just a simple educational project.
+
+## Compiling
+
+Just run `make` and you will have an iso at `build/crei.iso`.
+
+## Running
+
+Just run the `crei.iso` file in `build/` with this command:
+
+```bash
+$ qemu-system-x86_64 -cdrom build/crei.iso -m 256M
+```
--- src/main.c
+++ src/main.c
@@ -1,4 +1,19 @@
+void print_string(char *string_ptr) {
+    char *str = string_ptr;
+    
+    volatile char *video_memory = (volatile char*) 0xB8000;
+    
+    int i = 0;
+    while (str[i] != '\0') {
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = 0x0F;
+        
+        i++;
+    }
+}
+
 int main() {
+    print_string("Hello from Crei!");
     while(1) {}
 
     return 0;
